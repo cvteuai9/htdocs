3630b56b8ec4b0c1b3322a1988f040eda7965d7a
Cloud <cvteyai9@gmail.com>
2024-06-01 16:19:22 +0800

    新增分類頁籤(包含排序)_新增商品圖片

--- product/product-list.php
+++ product/product-list.php
@@ -2,7 +2,8 @@
 require_once("../db_connect.php");
 
 // 全部資料
-$sqlAll = "SELECT p.id, p.name AS product_name, p.price, p.created_at, tc.name AS tea_category_name, b.name AS brand_name, pack.name AS package_name, style.name AS style_name FROM product_category_relation pcr 
+$sqlAll = "SELECT p.id, p.name AS product_name, p.price, p.created_at, tc.name AS tea_category_name, b.name AS brand_name, pack.name AS package_name, style.name AS style_name, p_img.path FROM product_category_relation pcr 
+JOIN product_images p_img ON pcr.product_id = p_img.id
 JOIN products p ON pcr.product_id = p.id 
 JOIN brand b ON pcr.brand_id = b.id 
 JOIN tea_category tc ON pcr.tea_id = tc.id
@@ -14,8 +15,8 @@ $allCount = $resultAll->num_rows;
 // 一頁顯示10筆資料
 $perPage = 10;
 
-// 有搜尋條件時
 if (isset($_GET["search"]) && !empty($_GET["search"])) {
+    // 有搜尋條件時
     $search = $_GET["search"];
     $sql = "$sqlAll
     WHERE p.id LIKE '%$search%'
@@ -25,11 +26,50 @@ if (isset($_GET["search"]) && !empty($_GET["search"])) {
     OR pack.name LIKE '%$search%'
     OR style.name LIKE '%$search%'
     ORDER BY id ASC";
+    $result = $conn->query($sql);
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+    $productCount = $result->num_rows;
+} else if (isset($_GET["page"]) && isset($_GET["order"]) && isset($_GET["category"])) {
+    // 點擊分類頁籤時
+
+    $page = $_GET["page"];
+    $order = $_GET["order"];
+    $category = $_GET["category"];
+    $firstItem = ($page - 1) * $perPage;
+
+    // 所有符合該分類頁籤的資料筆數
+    $sqlCategory = "$sqlAll
+    WHERE tc.id = $category";
+    $resultCategory = $conn->query($sqlCategory);
+    $categoryCount = $resultCategory->num_rows;
+
+    // 依照order(排序)對應的sql語法
+    switch ($order) {
+        case 1:
+            $sql = "$sqlAll
+            WHERE tc.id = $category
+            ORDER BY id ASC LIMIT $firstItem, $perPage";
+            break;
+
+        case 2:
+            $sql = "$sqlAll
+            WHERE tc.id = $category
+            ORDER BY id DESC LIMIT $firstItem, $perPage";
+            break;
+    }
+    $result = $conn->query($sql);
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+    // 符合條件的商品總數productCount = 該分類頁籤下的商品總數categoryCount
+    $productCount = $categoryCount;
+    $pageCount = ceil($productCount / $perPage);
 } else if (isset($_GET["page"]) && isset($_GET["order"])) {
+    // 商品列表首頁
+
     $page = $_GET["page"];
     $order = $_GET["order"];
     $firstItem = ($page - 1) * $perPage;
-    $pageCount = ceil($allCount / $perPage);
+
     switch ($order) {
         case 1:
             $sql = "$sqlAll
@@ -41,16 +81,16 @@ if (isset($_GET["search"]) && !empty($_GET["search"])) {
             ORDER BY id DESC LIMIT $firstItem, $perPage";
             break;
     }
+    $result = $conn->query($sql);
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+    $productCount = $allCount;
+    $pageCount = ceil($productCount / $perPage);
 } else {
     header("location:product-list.php?page=1&order=1");
 }
 
-$result = $conn->query($sql);
-$rows = $result->fetch_all(MYSQLI_ASSOC);
-$productCount = $result->num_rows;
-if (isset($_GET["page"])) {
-    $productCount = $allCount;
-}
+
+
 ?>
 
 <!doctype html>
@@ -87,6 +127,11 @@ if (isset($_GET["page"])) {
 
         .main-content {
             margin: var(--header-height) 0 0 var(--aside-witch);
+        }
+
+        .product-img {
+            width: 100px;
+            height: 100px;
         }
     </style>
 </head>
@@ -149,19 +194,19 @@ if (isset($_GET["page"])) {
             <!-- 分類nav -->
             <ul class="nav nav-underline ps-2">
                 <li class="nav-item">
-                    <a class="nav-link text-success" href="">全部</a>
+                    <a class="nav-link text-success <?php if (!isset($_GET["category"])) echo "active" ?>" href="product-list.php?page=1&order=1">全部</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-success" href="">紅茶</a>
+                    <a class="nav-link text-success <?php if (isset($_GET["category"]) && $_GET["category"] == 1) echo "active" ?>" href="product-list.php?page=1&order=1&category=1">紅茶</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-success" href="">綠茶</a>
+                    <a class="nav-link text-success  <?php if (isset($_GET["category"]) && $_GET["category"] == 2) echo "active" ?>" href="product-list.php?page=1&order=1&category=2">綠茶</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-success" href="">青茶(烏龍茶)</a>
+                    <a class="nav-link text-success  <?php if (isset($_GET["category"]) && $_GET["category"] == 3) echo "active" ?>" href="product-list.php?page=1&order=1&category=3">青茶(烏龍茶)</a>
                 </li>
             </ul>
-            <hr class="my-1">
+            <hr class="my-3">
             <div class="row g-3 justify-content-between">
                 <div class="col-auto">
                     <!-- 搜尋表單 -->
@@ -169,7 +214,7 @@ if (isset($_GET["page"])) {
                         <div class="d-flex justify-content-start">
                             <?php if (isset($_GET["search"])) : //有搜尋條件時才會出現重置按鈕 
                             ?>
-                                <a href="product-list.php?page=1" class="btn btn-primary"><i class="fa-solid fa-arrow-rotate-left"></i></a>
+                                <a href="product-list.php?page=1&order=1" class="btn btn-primary"><i class="fa-solid fa-arrow-rotate-left"></i></a>
                             <?php endif; ?>
                             <input type="text" class="form-control" placeholder="Search..." name="search">
                             <button class="btn btn-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
@@ -179,26 +224,67 @@ if (isset($_GET["page"])) {
                 </div>
                 <div class="col-auto">
                     <div class="btn-group" role="group" aria-label="Basic example">
-                        <a href="?page=<?= $page ?>&order=1" class="btn btn-success <?php if ($order == 1) echo "active"; ?>">id <i class="fa-solid fa-arrow-down-short-wide"></i></a>
-                        <a href="?page=<?= $page ?>&order=2" class="btn btn-success <?php if ($order == 2) echo "active"; ?>">id <i class="fa-solid fa-arrow-down-wide-short"></i></a>
+                        <?php if (isset($_GET["category"])) : ?>
+                            <a href="?page=<?= $page ?>&order=1&category=<?= $category ?>" class="btn btn-success <?php if ($order == 1) echo "active"; ?>">id <i class="fa-solid fa-arrow-down-short-wide"></i></a>
+                            <a href="?page=<?= $page ?>&order=2&category=<?= $category ?>" class="btn btn-success <?php if ($order == 2) echo "active"; ?>">id <i class="fa-solid fa-arrow-down-wide-short"></i></a>
+                        <?php else : ?>
+                            <a href="?page=<?= $page ?>&order=1" class="btn btn-success <?php if ($order == 1) echo "active"; ?>">id <i class="fa-solid fa-arrow-down-short-wide"></i></a>
+                            <a href="?page=<?= $page ?>&order=2" class="btn btn-success <?php if ($order == 2) echo "active"; ?>">id <i class="fa-solid fa-arrow-down-wide-short"></i></a>
+                        <?php endif; ?>
                     </div>
                     <a href="" class="btn btn-success"><i class="fa-regular fa-square-plus"></i> 新增商品</a>
                 </div>
             </div>
         </div>
-        <div class="container-fluid py-3">
-            <div class="py-2 d-flex gap-3">
-                <div>
-                    共 <?= $productCount ?> 筆
+        <div class="container-fluid">
+            <div class="row justify-content-between">
+                <div class="col-auto py-2 d-flex gap-3">
+                    <div>
+                        共 <?= $productCount ?> 筆
+                    </div>
+                    <div>
+                        <?php if (isset($_GET["page"])) : ?>
+                            第 <?= $_GET["page"] ?> 頁 / 共 <?= $pageCount ?> 頁
+                        <?php else : ?>
+                            第 1 頁 / 共 1 頁
+                        <?php endif; ?>
+                    </div>
                 </div>
-                <div>
-                    <?php if (isset($_GET["page"])) : ?>
-                        第 <?= $_GET["page"] ?> 頁 / 共 <?= $pageCount ?> 頁
-                    <?php else : ?>
-                        第 1 頁 / 共 1 頁
+                <div class="col-auto">
+                    <!-- 如果選擇分類頁籤，每一個分頁按鈕外加固定category的值 -->
+                    <?php if (isset($_GET["page"]) && isset($_GET["category"])) : ?>
+                        <div class="d-flex justify-content-center">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination">
+                                    <?php for ($i = 1; $i <= $pageCount; $i++) : ?>
+                                        <li class="page-item <?php if ($i == $page) echo "active" ?>">
+                                            <a class="page-link" href="?page=<?= $i ?>&order=<?= $order ?>&category=<?= $category ?>">
+                                                <?= $i ?>
+                                            </a>
+                                        </li>
+                                    <?php endfor; ?>
+                                </ul>
+                            </nav>
+                        </div>
+                    <?php elseif (isset($_GET["page"])) : ?>
+                        <div class="d-flex justify-content-center">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination">
+                                    <?php for ($i = 1; $i <= $pageCount; $i++) : ?>
+                                        <li class="page-item <?php if ($i == $page) echo "active" ?>">
+                                            <a class="page-link" href="?page=<?= $i ?>&order=<?= $order ?>">
+                                                <?= $i ?>
+                                            </a>
+                                        </li>
+                                    <?php endfor; ?>
+                                </ul>
+                            </nav>
+                        </div>
+
                     <?php endif; ?>
                 </div>
             </div>
+
         </div>
         <div class="container-fluid">
             <!-- 商品表格 -->
@@ -219,7 +305,9 @@ if (isset($_GET["page"])) {
                         <?php foreach ($rows as $row) : ?>
                             <tr class="text-nowrap">
                                 <td><?= $row["id"] ?></td>
-                                <td>1</td>
+                                <td>
+                                    <img class="product-img object-fit-cover" src="<?= $row["path"] ?>" alt="">
+                                </td>
                                 <td><?= $row["product_name"] ?></td>
                                 <td><?= $row["brand_name"] ?></td>
                                 <td><?= $row["tea_category_name"] ?></td>
@@ -236,21 +324,6 @@ if (isset($_GET["page"])) {
                     </tbody>
                 </table>
 
-                <?php if (isset($_GET["page"])) : ?>
-                    <div class="d-flex justify-content-center">
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination">
-                                <?php for ($i = 1; $i <= $pageCount; $i++) : ?>
-                                    <li class="page-item <?php if ($i == $page) echo "active" ?>">
-                                        <a class="page-link" href="?page=<?= $i ?>&order=<?= $order ?>">
-                                            <?= $i ?>
-                                        </a>
-                                    </li>
-                                <?php endfor; ?>
-                            </ul>
-                        </nav>
-                    </div>
-                <?php endif; ?>
             <?php else : ?>
                 無符合條件的商品
             <?php endif; ?>
