8aaedb93399e79e8a4d93f12755d22fd087a67cc
Cloud <cvteyai9@gmail.com>
2024-06-07 10:57:10 +0800

    新增商品價錢排序&修改文章&商品表格樣式&移除user-text-bold

--- dashboard.php
+++ dashboard.php
@@ -8,14 +8,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <?php include("css.php") ?>
   <style>
+    .dashboard-bg {
+      background: url("tea.png") center center/contain no-repeat;
+    }
 
+    .main {
+      height: 80vh;
+    }
   </style>
 </head>
 
 <body>
   <?php include("dashboard-comm.php") ?>
   <main class="main-content p-3">
+    <div class="dashboard-bg container-fluid main">
 
+    </div>
   </main>
   <?php include_once("js.php") ?>
 </body>
